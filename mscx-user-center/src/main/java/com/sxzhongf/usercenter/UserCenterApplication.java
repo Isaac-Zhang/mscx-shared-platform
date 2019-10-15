e664fe2039c846e64b46661451b0dc7d73f9e54b
Isaac Zhang <zhangpan0614@126.com>
2019-10-15 16:34:43 +0800

    add swagger for user-center

--- mscx-user-center/src/main/java/com/sxzhongf/usercenter/UserCenterApplication.java
+++ mscx-user-center/src/main/java/com/sxzhongf/usercenter/UserCenterApplication.java
@@ -1,5 +1,6 @@
 package com.sxzhongf.usercenter;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,6 +15,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.sxzhongf")
 @SpringBootApplication
 //@EnableDiscoveryClient //在Nacos早期版本，必须添加注解
+@EnableSwagger2Doc
 public class UserCenterApplication {
 
     public static void main(String[] args) {
