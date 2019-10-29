98ec18492616db29ccaba523846b61044785497b
Isaac Zhang <zhangpan0614@126.com>
2019-10-29 17:18:00 +0800

    debug compareTo for colleague.

--- mscx-user-center/src/main/java/com/sxzhongf/usercenter/domain/entity/user/User.java
+++ mscx-user-center/src/main/java/com/sxzhongf/usercenter/domain/entity/user/User.java
@@ -1,6 +1,10 @@
 package com.sxzhongf.usercenter.domain.entity.user;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
 import javax.persistence.*;
 
 import lombok.*;
@@ -10,7 +14,9 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user")
-public class User {
+public class User
+//    implements Comparable<User>
+{
     /**
      * Id
      */
@@ -57,4 +63,50 @@ public class User {
      * 积分
      */
     private Integer bonus;
+
+//    public static void main(String[] args) {
+//        User user1 = new User();
+//        user1.setBonus(10);
+//
+//        User user2 = new User();
+//        user2.setBonus(20);
+//
+//        User user3 = new User();
+//        user3.setBonus(30);
+//
+//        User user4 = new User();
+//        user4.setBonus(40);
+//
+//        User user5 = new User();
+//        user5.setBonus(30);
+//
+//        User user6 = new User();
+//        user6.setBonus(60);
+//
+//
+//        List<User> list = new ArrayList<>();
+//        list.add(user3);
+//        list.add(user1);
+////        list.add(user2);
+////        list.add(user3);
+////        list.add(user5);
+////        list.add(user6);
+////        list.add(user4);
+//
+//
+//        Collections.sort(list);
+//
+//        System.out.println(list.get(0).getBonus()+"-----User:"+list.get(0));
+//    }
+//
+//    @Override
+//    public int compareTo(User o) {
+//        User t = this;
+//        if(o.getBonus()==30){
+//            return -1;//当前元素>20
+//        }
+//
+//        int r = this.getBonus().compareTo(o.getBonus());
+//        return -r;
+//    }
 }
